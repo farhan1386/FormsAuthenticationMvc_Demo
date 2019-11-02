4219b3e5da7a9bc961d80978611ae792b8eb113a
Farhan Ahmed <farhan1086@gmail.com>
2019-11-02 09:38:31 +0530

    updated

---
+++ FormsAuthenticationMvc_Demo/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using FormsAuthenticationMvc_Demo.Models;
+using FormsAuthenticationMvc_Demo.ViewModel;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FormsAuthenticationMvc_Demo.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly EmployeeContext db = new EmployeeContext();
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserViewModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                bool IsValidUser = db.Users.Any(user => user.UserName.ToLower() ==
+                userModel.UserName.ToLower() && user.UserPassword == userModel.UserPassword);
+
+                if (IsValidUser)
+                {
+                    FormsAuthentication.SetAuthCookie(userModel.UserName, false);
+                    return RedirectToAction("Index", "Employees");
+                }
+                ModelState.AddModelError("","Invalid username and password");
+            }
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ FormsAuthenticationMvc_Demo/ViewModel/UserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormsAuthenticationMvc_Demo.ViewModel
+{
+    public class UserViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please enter username")]
+        [Display(Name ="Username")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter password")]
+        [Display(Name = "Password")]
+        public string UserPassword { get; set; }
+    }
+}
